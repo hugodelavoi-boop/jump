88dbbd0a6c7b9744727e8ef020cd42d9c576c47d
hugodelavoi-boop <hugodelavoi@gmail.com>
2025-08-28 14:22:08 +0800

    Updated 20250828062100_icy_shadow.sql

--- supabase/migrations/20250828062100_icy_shadow.sql
+++ supabase/migrations/20250828062100_icy_shadow.sql
@@ -12,16 +12,5 @@
  */
  
 -INSERT INTO schools (name, suburb, postcode) VALUES
--  ('Subiaco Primary School', 'Subiaco', '6008'),
--  ('Leederville Primary School', 'Leederville', '6007'),
--  ('Mount Hawthorn Primary School', 'Mount Hawthorn', '6016'),
--  ('North Perth Primary School', 'North Perth', '6006'),
--  ('West Leederville Primary School', 'West Leederville', '6007'),
--  ('Churchlands Primary School', 'Churchlands', '6018'),
--  ('Floreat Park Primary School', 'Floreat', '6014'),
--  ('Wembley Primary School', 'Wembley', '6014'),
--  ('City Beach Primary School', 'City Beach', '6015'),
--  ('Scarborough Primary School', 'Scarborough', '6019')
-+INSERT INTO schools (name, suburb, postcode) VALUES
 +  ('Tester School', 'suburb', '1234')
  ON CONFLICT (name) DO NOTHING;
