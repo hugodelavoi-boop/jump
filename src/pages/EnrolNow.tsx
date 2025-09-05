9ece7f0278d466c3bc82d8e37ec5b4fe29190f15
hugodelavoi-boop <hugodelavoi@gmail.com>
2025-09-05 19:28:56 +0800

    Fix missing useEffect import on enrol page

--- src/pages/EnrolNow.tsx
+++ src/pages/EnrolNow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import AuthWrapper from '../components/AuthWrapper';
 import ConsentWaiver from '../components/ConsentWaiver';
